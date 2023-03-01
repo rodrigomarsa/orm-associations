6cc2fcf10d0f2f93494637101d842181f771e6c3
Rodrigo <rodrigomartins.agro@gmail.com>
2023-03-01 18:47:45 -0300

    add managed transaction function

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -47,7 +47,26 @@ const getById = async (id) => {
 //     });
 // });
 
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const employee = await Employee.create({
+      firstName, lastName, age
+    }, { transaction: t });
+
+    await Address.create({
+      city, street, number, employeeId: employee.id
+    }, { transaction: t });
+    return employee;
+  });
+
+  return result;
+  // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+  // não sendo necessário finalizar a transação manualmente.
+  // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+};
+
 module.exports = {
   getAll,
   getById,
+  insert,
  };
